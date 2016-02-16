0b4ef84a4ccb1513f998aef5e04bd6fab861f894
Giuseppe Scrivano <gscrivan@redhat.com>
2016-02-16 13:58:53 +0100

    Add uninstall --spc
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- Atomic/atomic.py
+++ Atomic/atomic.py
@@ -576,6 +576,9 @@ class Atomic(object):
         self._rpmostree(argv)
 
     def uninstall(self):
+        if self.args.spc:
+            return self._uninstallspc()
+
         self.inspect = self._inspect_container()
         if self.inspect and self.force:
             self.force_delete_containers()
@@ -778,6 +781,31 @@ class Atomic(object):
 
         if not self.args.display:
             return util.check_call(cmd)
+
+    def systemctl_command(self, cmd):
+        cmd = self.sub_env_strings(self.gen_cmd(["systemctl", cmd, self.image]))
+        self.display(cmd)
+        if not self.args.display:
+            util.check_call(cmd, env=self.cmd_env())
+
+    def _uninstallspc(self):
+        spcdir = os.path.realpath("/var/spc/%s" % self.image)
+        service_installed = os.path.exists(os.path.join(spcdir, "rootfs/exports/service.template"))
+        self.args.display = False
+        if service_installed:
+            self.systemctl_command("stop")
+            self.systemctl_command("disable")
+
+        if service_installed:
+            os.unlink("/usr/local/lib/systemd/system/%s.service" % (self.image))
+
+        exportedfs = os.path.join(spcdir, "rootfs/exports/rootfs/")
+        for root, _, files in os.walk(exportedfs):
+            for f in files:
+                os.unlink("/" + os.path.relpath(os.path.join(root, f), exportedfs))
+
+        os.unlink("/var/spc/%s" % self.image)
+        shutil.rmtree("/var/spc/%s.0" % self.image)
 
     def _installspc(self):
         self._check_if_image_present()
