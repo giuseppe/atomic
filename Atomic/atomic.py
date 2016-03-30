ee521fea51f70f8528245178e04a31d352489ef3
Giuseppe Scrivano <gscrivan@redhat.com>
2016-03-30 16:54:52 +0200

    Support importing images from an OSTree repo
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- Atomic/atomic.py
+++ Atomic/atomic.py
@@ -307,6 +307,8 @@ class Atomic(object):
 
     def pull_image(self):
         repo = self._get_ostree_repo()
+        if self.args.ostree:
+            self._check_oci_ostree_image(repo, "ostree://%s" % self.image, True)
         if self.args.docker:
             self._check_oci_docker_image(repo, "docker://%s" % self.image, True)
         elif self.args.tar:
@@ -1021,6 +1023,14 @@ class Atomic(object):
 
         repo.commit_transaction(None)
 
+    def _check_oci_ostree_image(self, repo, img, upgrade):
+        imagebranch = img.replace("ostree://", "")
+        current_rev = repo.resolve_rev(imagebranch, True)
+        if not upgrade and current_rev[1]:
+            return False
+        remote, branch = imagebranch.split(":")
+        return repo.pull(remote, [branch], 0, None)
+
     def _check_oci_docker_image(self, repo, img, upgrade):
         regloc, image, tag = self._parse_imagename(img.replace("docker://", ""))
         imagebranch = "dockerimg/%s-%s" % (image.replace("sha256:", ""), tag)
@@ -1065,8 +1075,11 @@ class Atomic(object):
         return metadata[key]
 
     def _checkout_oci(self, repo, name, img, deployment, upgrade):
-        regloc, image, tag = self._parse_imagename(img.replace("docker://", ""))
-        imagebranch = "dockerimg/%s-%s" % (image.replace("sha256:", ""), tag)
+        if "docker://" in img:
+            regloc, image, tag = self._parse_imagename(img.replace("docker://", ""))
+            imagebranch = "dockerimg/%s-%s" % (image.replace("sha256:", ""), tag)
+        elif "ostree://" in img:
+            imagebranch = img.replace("ostree://", "")
 
         destination = "/var/lib/containers/atomic/%s.%d" % (self.name, deployment)
         self.writeOut("Extracting to %s" % destination)
@@ -1088,15 +1101,22 @@ class Atomic(object):
         rev = repo.resolve_rev(imagebranch, False)[1]
 
         manifest = self._get_commit_metadata (repo, rev, "docker.manifest")
-        layers = self._get_layers_from_manifest(manifest)
-
         options = OSTree.RepoCheckoutOptions()
-        options.overwrite_mode = OSTree.RepoCheckoutOverwriteMode.UNION_FILES
-        rootfs_fd = os.open(rootfs, os.O_DIRECTORY)
-        for layer in layers:
-            rev = repo.resolve_rev("dockerimg/%s" % layer.replace("sha256:", ""), False)[1]
-            repo.checkout_tree_at(options, rootfs_fd, rootfs, rev)
-        os.close(rootfs_fd)
+        options.overwrite_mode = OSTree.RepoCheckoutOverwriteMode.UNION_FILES 
+
+        rootfs_fd = None
+        try:
+            rootfs_fd = os.open(rootfs, os.O_DIRECTORY)
+            if manifest is None:
+                repo.checkout_tree_at(options, rootfs_fd, rootfs, rev)
+            else:
+                layers = self._get_layers_from_manifest(manifest)
+                for layer in layers:
+                    rev_layer = repo.resolve_rev("dockerimg/%s" % layer.replace("sha256:", ""), False)[1]
+                    repo.checkout_tree_at(options, rootfs_fd, rootfs, rev_layer)
+        finally:
+            if rootfs_fd:
+                os.close(rootfs_fd)
 
         exports = os.path.join(destination, "rootfs/exports")
 
@@ -1153,10 +1173,12 @@ class Atomic(object):
     def _install_oci_container(self):
         repo = self._get_ostree_repo()
 
-        if not self.image.startswith("docker://"):
-            raise ValueError("Image type for '%s' not known. Only docker:// is supported." % self.image)
-
-        self._check_oci_docker_image(repo, self.image, False)
+        if self.image.startswith("docker://"):
+            self._check_oci_docker_image(repo, self.image, False)
+        elif self.image.startswith("ostree://"):
+            self._check_oci_ostree_image(repo, self.image, False)
+        else:
+            raise ValueError("Image type for '%s' not known. Only docker:// and ostree:// are supported." % self.image)
 
         if os.path.exists("/var/lib/containers/atomic/%s.0" % self.name):
             self.writeOut("/var/lib/containers/atomic/%s.0 already present" % self.name)
