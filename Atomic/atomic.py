giuseppe/atomic

commit d29c801ee78597b0ef3f9773467882da8ac9ec71

atomic: use fully qualified image names to Skopeo
Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

Closes: #434
Approved by: rhatdan

Atomic/atomic.py
@@ -987,20 +987,24 @@ class Atomic(object):
         else:
             return reg, image, "latest"
 
-    @staticmethod
-    def _convert_to_skopeo(image):
-        i = image.replace("oci:", "")
-        if "http:" in i:
-            return ["--insecure"], "docker://" + i.replace("http:", "")
+    def _convert_to_skopeo(self, image):
+        insecure = "http:" in image
+
+        for i in ["oci:", "http:", "https:"]:
+            image = image.replace(i, "")
+
+        fqn_image = self.find_remote_image(image)
+        if insecure:
+            return ["--insecure"], "docker://" + fqn_image
         else:
-            return [], "docker://" + i.replace("https:", "")
+            return [], "docker://" + fqn_image
 
     def _skopeo_get_manifest(self, image):
-        args, img = Atomic._convert_to_skopeo(image)
+        args, img = self._convert_to_skopeo(image)
         return util.skopeo_inspect(img, args)
 
     def _skopeo_get_layers(self, image, layers):
-        args, img = Atomic._convert_to_skopeo(image)
+        args, img = self._convert_to_skopeo(image)
         return util.skopeo_layers(img, args, layers)
 
     @staticmethod
@@ -1623,15 +1627,18 @@ class Atomic(object):
 
         return self.active_containers
 
-    def find_remote_image(self):
+    def find_remote_image(self, image=None):
         """
         Based on the user's input, see if we can associate the input with a remote
         registry and image.
         :return: str(fq name)
         """
-        results = self.d.search(self.image)
+        if image == None:
+            image = self.image
+
+        results = self.d.search(image)
         for x in results:
-            if x['name'] == self.image:
+            if x['name'] == image:
                 return '{}/{}'.format(x['registry_name'], x['name'])
         return None
 

commit 966db2f140e552ad537a9ad99eea534e236b51f1

Work correctly with fully qualified image names
Atomic.find_remote_image can return None if the image name provided by
the user has not the same name as the one found.  In that case, use the
name provided by the user.

Problem introduced by d29c801ee78597b0ef3f9773467882da8ac9ec71

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

Atomic/atomic.py
@@ -995,7 +995,7 @@ class Atomic(object):
         for i in ["oci:", "http:", "https:"]:
             image = image.replace(i, "")
 
-        fqn_image = self.find_remote_image(image)
+        fqn_image = self.find_remote_image(image) or image
         if insecure:
             return ["--insecure"], "docker://" + fqn_image
         else:

tests/integration/test_system_containers.sh
@@ -28,6 +28,8 @@ docker save atomic-test-system > ${WORK_DIR}/atomic-test-system.tar
 
 ${ATOMIC} pull dockertar:/${WORK_DIR}/atomic-test-system.tar
 
+${ATOMIC} pull docker.io/busybox
+
 # Check that the branch is created in the OSTree repository
 ostree --repo=${ATOMIC_OSTREE_REPO} refs | grep -q "ociimage/atomic-test-system-latest"
 
@@ -93,6 +95,7 @@ test \! -e /etc/systemd/system/${NAME}.service
 
 # check that there are not any "ociimage/" prefixed branch left after images --prune
 ostree --repo=${ATOMIC_OSTREE_REPO} refs --delete "ociimage/atomic-test-system-latest"
+ostree --repo=${ATOMIC_OSTREE_REPO} refs --delete "ociimage/busybox-latest"
 ${ATOMIC} images --prune
 OUTPUT=$(! ostree --repo=${ATOMIC_OSTREE_REPO} refs | grep -c ociimage)
 if test $OUTPUT \!= 0; then