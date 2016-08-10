giuseppe/atomic

commit 63024095faac181169f16ec0105b587092106709

Implementing Atomic ps for DBus API
Closes: #516
Approved by: giuseppe

Atomic/ps.py
@@ -8,53 +8,57 @@ from . import atomic
 import datetime
 
 class Ps(Atomic):
-    def ps(self):
+
+    def ps_tty(self):
+        skull = (u"\u2620").encode('utf-8')
+        all_container_info = self.ps()
         all_containers = []
+        for each in all_container_info:
+            if each["Type"] == "systemcontainer":
+                container = each["Id"]
+                status = "exited"
+                created = datetime.datetime.fromtimestamp(each["Created"])
+                try:
+                    inspect_stdout = util.check_output(["runc", "state", container], stderr=atomic.DEVNULL)
+                    ret = json.loads(inspect_stdout.decode())
+                    status = ret["status"]
+                    created = dateparse(ret['created'])
+                except (subprocess.CalledProcessError):
+                    pass
 
-        # Collect the system containers
-        for i in self.syscontainers.get_system_containers():
-            container = i["Id"]
-
-            status = "exited"
-            created = datetime.datetime.fromtimestamp(i["Created"])
-            try:
-                inspect_stdout = util.check_output(["runc", "state", container], stderr=atomic.DEVNULL)
-                ret = json.loads(inspect_stdout.decode())
-                status = ret["status"]
-                created = dateparse(ret['created'])
-            except (subprocess.CalledProcessError):
-                pass
-
-            if not self.args.all and status != "running":
-                continue
-
-            image = i['Image']
-            command = i['Command']
-            created = created.strftime("%F %H:%M") # pylint: disable=no-member
-            container_info = {"type" : "systemcontainer", "container" : container,
-                              "image" : image, "command" : command, "created" : created,
-                              "status" : status, "runtime" : "runc"}
-
-            if self.args.filter:
-                if not self._filter_include_container(container_info):
+                if not self.args.all and status != "running":
                     continue
-            all_containers.append(container_info)
 
-        # Collect the docker containers
-        for container in [x["Id"] for x in self.d.containers(all=self.args.all)]:
-            ret = self._inspect_container(name=container)
-            status = ret["State"]["Status"]
-            image = ret['Config']['Image']
-            command = u' '.join(ret['Config']['Cmd']) if ret['Config']['Cmd'] else ""
-            created = dateparse(ret['Created']).strftime("%F %H:%M") # pylint: disable=no-member
-            container_info = {"type" : "docker", "container" : container,
+                image = each['Image']
+                command = each["Command"]
+                created = created.strftime("%F %H:%M") # pylint: disable=no-member
+                container_info = {"type" : "systemcontainer", "container" : container,
                               "image" : image, "command" : command,
-                              "created" : created, "status" : status, "runtime" : "Docker"}
+                              "created" : created, "status" : status,
+                              "runtime" : "runc", "vulnerable" : each["vulnerable"]}
 
-            if self.args.filter:
-                if not self._filter_include_container(container_info):
-                    continue
-            all_containers.append(container_info)
+                if self.args.filter:
+                    if not self._filter_include_container(container_info):
+                        continue
+                all_containers.append(container_info)
+
+            elif each["Type"] == "docker":
+            # Collect the docker containers
+                for container in [x["Id"] for x in self.d.containers(all=self.args.all)]:
+                    ret = self._inspect_container(name=container)
+                    status = ret["State"]["Status"]
+                    image = ret['Config']['Image']
+                    command = u' '.join(ret['Config']['Cmd']) if ret['Config']['Cmd'] else ""
+                    created = dateparse(ret['Created']).strftime("%F %H:%M") # pylint: disable=no-member
+                    container_info = {"type" : "docker", "container" : container,
+                              "image" : image, "command" : command,
+                              "created" : created, "status" : status,
+                              "runtime" : "Docker", "vulnerable" : each["vulnerable"]}
+
+                    if self.args.filter:
+                        if not self._filter_include_container(container_info):
+                            continue
+                    all_containers.append(container_info)
 
         if not all_containers:
             return
@@ -77,10 +81,11 @@ class Ps(Atomic):
                 util.write_out(container["container"][0:max_len_container])
             return
 
-        col_out = "{0:%s} {1:%s} {2:%s} {3:16} {4:9} {5:10}" % (max_len_container, max_len_image, max_len_command)
+        col_out = "{0:2} {1:%s} {2:%s} {3:%s} {4:16} {5:9} {6:10}" % (max_len_container, max_len_image, max_len_command)
 
         if self.args.heading:
-            util.write_out(col_out.format("CONTAINER ID",
+            util.write_out(col_out.format(" ",
+                                          "CONTAINER ID",
                                           "IMAGE",
                                           "COMMAND",
                                           "CREATED",
@@ -89,12 +94,45 @@ class Ps(Atomic):
 
         #if self.args.truncate:
         for container in all_containers:
-            util.write_out(col_out.format(container["container"][0:max_len_container],
+            indicator = ""
+            if container["vulnerable"]:
+                if util.is_python2:
+                    indicator = indicator + skull + " "
+                else:
+                    indicator = indicator + str(skull, "utf-8") + " "
+            util.write_out(col_out.format(indicator,
+                                          container["container"][0:max_len_container],
                                           container["image"][0:max_len_image],
                                           container["command"][0:max_len_command],
                                           container["created"][0:16],
                                           container["status"][0:9],
                                           container["runtime"][0:10]))
+    def ps(self):
+        all_containers = []
+        vuln_ids = self.get_vulnerable_ids()
+        all_vuln_info = json.loads(self.get_all_vulnerable_info())
+
+        # Collect the system containers
+        for i in self.syscontainers.get_system_containers():
+            i["vulnerable"] = i['Id'] in vuln_ids
+            if i["vulnerable"]:
+                i["vuln_info"] = all_vuln_info[i['Id']]
+            else:
+                i["vuln_info"] = dict()
+            all_containers.append(i)
+
+        # Collect the docker containers
+        for container in [x["Id"] for x in self.d.containers(all=self.args.all)]:
+            ret = self._inspect_container(name=container)
+            ret["Type"] = "docker"
+            ret["vulnerable"] = ret["Image"] in vuln_ids
+            if ret["vulnerable"]:
+                ret["vuln_info"] = all_vuln_info[ret["ImageId"]]
+            else:
+                ret["vuln_info"] = dict()
+            all_containers.append(ret)
+
+        return all_containers
 
     def _filter_include_container(self, container_info):
         filterables = ["container", "image", "command", "created", "status", "runtime"]

Atomic/syscontainers.py
@@ -80,7 +80,7 @@ class SystemContainers(object):
         # Accessing .disable_fsync and .process_whiteouts thus results in a segfault in
         # libostree.  Re-enable this once it gets fixed.
         if OSTREE_SAFE_GLIB_REPO_CHECKOUT_OPTIONS:
-            options = OSTree.RepoCheckoutOptions() # pylint: disable=no-member 
+            options = OSTree.RepoCheckoutOptions() # pylint: disable=no-member
             options.overwrite_mode = OSTree.RepoCheckoutOverwriteMode.UNION_FILES
             options.process_whiteouts = True
             options.disable_fsync = True
@@ -399,7 +399,7 @@ class SystemContainers(object):
                 command = u' '.join(config["process"]["args"])
 
             container = {'Image' : image, 'ImageID' : revision, 'Id' : x, 'Created' : created, 'Names' : [x],
-                         'Command' : command}
+                         'Command' : command, 'Type' : 'systemcontainer'}
             ret.append(container)
         return ret
 

atomic
@@ -486,7 +486,7 @@ def create_parser(help_text):
     pss = subparser.add_parser(
         "ps", help=_("list the containers"),
         epilog="By default this shows only the running containers.")
-    pss.set_defaults(_class=Ps, func='ps')
+    pss.set_defaults(_class=Ps, func='ps_tty')
     pss.add_argument("-a", "--all", action='store_true',dest="all", default=False,
                      help=_("show all containers"))
     pss.add_argument("-f", "--filter", metavar='FILTER', action='append', dest="filter",

atomic_client.py
@@ -77,6 +77,9 @@ class AtomicDBus (object):
     def vulnerable(self):
         return self.dbus_object.VulnerableInfo(dbus_interface="org.atomic", timeout = 2147400)
 
+    @polkit.enable_proxy
+    def ps(self):
+        return self.dbus_object.Ps(dbus_interface="org.atomic", timeout = 2147400)
 #For outputting the list of scanners
 def print_scan_list(all_scanners):
     if len(all_scanners) == 0:
@@ -164,6 +167,9 @@ if __name__ == "__main__":
 
         elif(sys.argv[1] == "vulnerable"):
             print(json.loads(dbus_proxy.vulnerable()))
+
+        elif(sys.argv[1] == "ps"):
+            print(json.loads(dbus_proxy.ps()))
 
     except dbus.DBusException as e:
         print (e)

atomic_dbus.py
@@ -13,6 +13,7 @@ from Atomic.verify import Verify
 from Atomic.storage import Storage
 from Atomic.diff import Diff
 from Atomic.scan import Scan
+from Atomic.ps import Ps
 
 class atomic_dbus(slip.dbus.service.Object):
     default_polkit_auth_required = "org.atomic.readwrite"
@@ -263,6 +264,16 @@ class atomic_dbus(slip.dbus.service.Object):
         args = self.Args()
         self.atomic.set_args(args)
         return self.atomic.get_all_vulnerable_info()
+
+    # The Ps method will list all containers on the system.
+    @slip.dbus.polkit.require_auth("org.atomic.read")
+    @dbus.service.method("org.atomic", in_signature='', out_signature='s')
+    def Ps(self):
+        ps = Ps()
+        ps.useTTY = False
+        args = self.Args()
+        ps.set_args(args)
+        return json.dumps(ps.ps())
 
 
 if __name__ == "__main__":

tests/integration/test_system_containers.sh
@@ -68,19 +68,19 @@ trap teardown EXIT
 
 ${ATOMIC} --debug install --name=${NAME} --set=RECEIVER=${SECRET} --system oci:atomic-test-system
 
-${ATOMIC} ps > ps.out
+${ATOMIC} --debug ps --no-trunc > ps.out
 grep -q "test-system" ps.out
-${ATOMIC} ps --json > ps.out
+${ATOMIC} --debug ps --json > ps.out
 grep -q "test-system" ps.out
 ${ATOMIC} ps --all > ps.out
 grep -q "test-system" ps.out
-${ATOMIC} ps --json --all > ps.out
+${ATOMIC} ps --all --no-trunc > ps.out
 grep -q "test-system" ps.out
-${ATOMIC} ps --filter id=test-system > ps.out
+${ATOMIC} ps --no-trunc --filter id=test-system > ps.out
 grep -q "test-system" ps.out
 ${ATOMIC} ps --no-trunc > ps.out
 grep -q "test-system" ps.out
-${ATOMIC} ps --quiet > ps.out
+${ATOMIC} ps --no-trunc --quiet > ps.out
 grep -q "test-system" ps.out
 ${ATOMIC} ps -aq --no-trunc --filter id=test-system > ps.out
 grep -q "test-system" ps.out

commit 7c5a754f8fcf271aea441ca405aa8e8969b10ef7

ps: do not show Docker containers multiple times
regression introduced with 63024095faac181169f16ec0105b587092106709

Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

Atomic/ps.py
@@ -40,21 +40,21 @@ class Ps(Atomic):
 
             elif each["Type"] == "docker":
             # Collect the docker containers
-                for container in [x["Id"] for x in self.d.containers(all=self.args.all)]:
-                    ret = self._inspect_container(name=container)
-                    status = ret["State"]["Status"]
-                    image = ret['Config']['Image']
-                    command = u' '.join(ret['Config']['Cmd']) if ret['Config']['Cmd'] else ""
-                    created = dateparse(ret['Created']).strftime("%F %H:%M") # pylint: disable=no-member
-                    container_info = {"type" : "docker", "container" : container,
-                              "image" : image, "command" : command,
-                              "created" : created, "status" : status,
-                              "runtime" : "Docker", "vulnerable" : each["vulnerable"]}
+                container = each["Id"]
+                ret = self._inspect_container(name=container)
+                status = ret["State"]["Status"]
+                image = ret['Config']['Image']
+                command = u' '.join(ret['Config']['Cmd']) if ret['Config']['Cmd'] else ""
+                created = dateparse(ret['Created']).strftime("%F %H:%M") # pylint: disable=no-member
+                container_info = {"type" : "docker", "container" : container,
+                                  "image" : image, "command" : command,
+                                  "created" : created, "status" : status,
+                                  "runtime" : "Docker", "vulnerable" : each["vulnerable"]}
 
-                    if self.args.filter:
-                        if not self._filter_include_container(container_info):
-                            continue
-                    all_containers.append(container_info)
+                if self.args.filter:
+                    if not self._filter_include_container(container_info):
+                        continue
+                all_containers.append(container_info)
 
         if not all_containers:
             return

tests/integration/test_ps.sh
@@ -1,0 +1,26 @@
+#!/bin/bash -x
+set -euo pipefail
+IFS=$'\n\t'
+
+# Test scripts run with PWD=tests/..
+
+# The test harness exports some variables into the environment during
+# testing: PYTHONPATH (python module import path
+#          WORK_DIR   (a directory that is safe to modify)
+#          DOCKER     (the docker executable location)
+#          ATOMIC     (an invocation of 'atomic' which measures code coverage)
+#          SECRET     (a generated sha256 hash inserted into test containers)
+
+# In addition, the test harness creates some images for use in testing.
+#   See tests/test-images/
+
+OUTPUT=$(/bin/true)
+
+${ATOMIC} ps --all -q -f type=docker | sort > atomic.ps.out
+docker ps --all -q | sort > docker.ps.out
+diff docker.ps.out atomic.ps.out
+
+${ATOMIC} ps -q -f type=docker | sort > atomic.ps.out
+docker ps -q | sort > docker.ps.out
+diff docker.ps.out atomic.ps.out
+